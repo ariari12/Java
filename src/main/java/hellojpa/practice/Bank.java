c4878f8ec31cee4b1305e068b20222738e576485
kah <rladkgus4553@gmail.com>
2023-10-22 17:24:11 +0900

    클래스 인스턴스 연습

--- src/main/java/hellojpa/practice/Bank.java
+++ src/main/java/hellojpa/practice/Bank.java
@@ -1,4 +1,23 @@
 package hellojpa.practice;
 
 public class Bank {
+    static int balance=0;
+    public static void main(String[] args){
+        deposit(1000); //입금
+        checkBalance(); //출력
+        withdraw(500); // 출금
+        checkBalance();
+    }
+
+    private static void checkBalance() {
+        System.out.println("balance = " + balance);
+    }
+
+    private static void withdraw(int money) {
+        balance-=money;
+    }
+
+    private static void deposit(int money) {
+        balance +=money;
+    }
 }
